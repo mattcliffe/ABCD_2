eb738395f1afffbeb090f080183101377e2ec1af
Unknown <mattjcliffe@gmail.com>
2019-04-09 15:14:10 +0100

    Added crystal thickness
    added length of the particle to the calc

--- ABCD_2.m
+++ ABCD_2.m
@@ -47,11 +47,12 @@ for lmn = 1:90;
     
     phi = lmn*pi/180;
     BIREF = [exp(i*phi) 0 ; 0 exp(-i*phi)];
-
+    
     Eout = RotMatM2*  HorzPol * QWPN * RotMat2 * BIREF *RotMatM*   QWPP * HorzPol  * RotMat * Ein;
     Ex(lmn) = Eout(1);
     Ey(lmn) = Eout(2);
     E(lmn) = abs(Eout(1)).^2 + abs(Eout(2)).^2;
+    
 end
 
 figure(2)
@@ -59,6 +60,56 @@ clf()
 plot(abs(Ex).^2,'k');
 hold on
 plot(abs(Ey).^2,'r')
-hold on 
+hold on
 plot(E,'b')
-legend('|E_x|^2','|E_y|^2','E')
+legend('|E_x|^2','|E_y|^2','|E|^2')
+grid('on')
+xlabel('Phase rotation [deg]')
+ylabel('Signal [arb.]')
+
+
+%% Added length to phase rotation
+% Assume propagation exp(-\alpha * Z)
+% \alpha is phase rotation, Z is propagation distance
+%Changed to realistic phase rotations and lengths
+clear all; clc
+Ein = [0.9 ;0.1];
+HorzPol = [1 0 ; 0 0];
+QWPP = [exp(i*pi/4) 0 ; 0 i*exp(i*pi/4)];
+QWPN = [-exp(-i*pi/4) 0 ; 0 -i*exp(i*pi/4)];
+
+for lmn = 1:90; %Phase rotation loop microrad
+    for prq = 1:50 %distance loop microns
+        
+        z = (prq-1)./1e3;
+        phi = exp((((lmn-1)*1e-3)*pi/180)*z);
+        
+        Ang =45*(pi/180);%lmn*pi/180;
+        RotMat = [cos(Ang) -sin(Ang);sin(Ang) cos(Ang)];
+        RotMatM = [cos(-Ang) -sin(-Ang);sin(-Ang) cos(-Ang)];
+        
+        Ang2 = Ang+(90*pi/180);%Ang + (90*pi/180);
+        RotMat2 = [cos(Ang2) -sin(Ang2);sin(Ang2) cos(Ang2)];
+        RotMatM2 = [cos(-Ang2) -sin(-Ang2);sin(-Ang2) cos(-Ang2)];
+        
+        BIREF = [phi 0 ; 0 -phi];
+        
+        Eout = RotMatM2*  HorzPol * QWPN * RotMat2 * BIREF *RotMatM*   QWPP * HorzPol  * RotMat * Ein;
+        Ex(lmn,prq) = Eout(1);
+        Ey(lmn,prq) = Eout(2);
+        E(lmn,prq) = abs(Eout(1)).^2 + abs(Eout(2)).^2;
+        zplot(prq) = z*1e6;
+    end
+    
+    Phiplot(lmn) = (lmn)*pi/180;
+    
+    
+end
+figure(2)
+clf
+pcolor(zplot,Phiplot,E)
+xlabel('Propagation Distance [{\mu}m]')
+ylabel('Phase rotation')
+shading interp
+colorbar
+
